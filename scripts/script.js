5efed181a983a2c50d1969f8ef907045cf3421da
Hayyuu <hayatahimed518@gmail.com>
2023-05-28 12:26:11 +0300

    add a function that plays a single round

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,48 @@
+
+function playRound(playerChoice,computerChoice){
+    let winner,loser;
+    let rock="ROCK";
+    let paper="PAPER";
+    let scissor="SCISSOR";
+    playerChoice=playerChoice.toUpperCase();
+    computerChoice=computerChoice.toUpperCase();
+    if(playerChoice==computerChoice){
+        winner="No winner";
+    }
+    else if(playerChoice==rock){
+        if(computerChoice==scissor){
+            winner=rock;
+            loser=scissor;
+        }
+        else{
+            winner=paper;
+            loser=rock;
+        }
+    }
+    else if(playerChoice==scissor){
+        if(computerChoice==rock){
+            winner=rock;
+            loser=scissor;
+        }
+        else{
+            winner=scissor;
+            loser=paper;
+        }
+    }
+    else{
+        if(computerChoice==rock){
+            winner=paper;
+            loser=rock;
+        }
+        else{
+            winner=scissor;
+            loser=paper;
+        }
+    }
+    return winner;
+
+}
+
 function getComputerChoice(){
     let ranNumber=Math.floor(Math.random()* 10);
     let choice;
@@ -6,3 +51,7 @@ function getComputerChoice(){
     else choice="Scissor";
     return choice;
 }
+const playerChoice="paper";
+const computerChoice=getComputerChoice();
+console.log(`${playerChoice} , ${computerChoice}`);
+console.log(playRound(playerChoice,computerChoice));
